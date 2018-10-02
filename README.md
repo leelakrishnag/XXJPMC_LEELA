e28e7edba62e08b55587e9749f45b60d360b8a3e
Your Name <you@example.com>
2018-10-02 15:59:21 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Super Simple Stocks
+
+## Tests
+
+A moderately extensive (although my no means exhaustive) suite of tests is included in `tests/`. 
+The autodiscovery feature of `unittest` makes it fairly convenient to run them by executing the following command:
+ ````
+$ git clone https://github.com/aadroher/super_simple_stocks
+$ cd LEEJPM/
+$ python -m unittest -v
+````
+The `-v` switch is optional and it stands for its verbose mode.
+
+
+
+
+
+
---
+++ tests/test_xxjpmc_gbce.py
@@ -0,0 +1,52 @@
+import unittest
+
+from xxjpmc_simplestock_req import GlobalBeverageCorporationExchange, TickerSymbol
+from .xxjpmc_fact import StockFactory, TradeFactory
+
+
+class GlobalBeverageCorporationExchangeInitTestCase(unittest.TestCase):
+
+    def test_checks_empty_stocks(self):
+        with self.assertRaises(ValueError):
+            gbce = GlobalBeverageCorporationExchange([])
+
+
+class GlobalBeverageCorporationExchangeRecordAllShareIndexTestCase(unittest.TestCase):
+
+    def test_not_enough_significant_trades_returns_none(self):
+        stocks = StockFactory.get_stocks()
+        gbce = GlobalBeverageCorporationExchange(stocks)
+        index = gbce.all_share_index()
+        self.assertIsNone(index)
+
+    def test_index_value(self):
+
+        tea_stock = StockFactory.get_stock_by_ticker_symbol(TickerSymbol.TEA)
+        gin_stock = StockFactory.get_stock_by_ticker_symbol(TickerSymbol.GIN)
+        gbce = GlobalBeverageCorporationExchange([tea_stock, gin_stock])
+
+        tea_stock_trades = TradeFactory.get_trades_for_stock(TickerSymbol.TEA)
+        gin_stock_trades = TradeFactory.get_trades_for_stock(TickerSymbol.GIN)
+
+        for trade in tea_stock_trades + gin_stock_trades:
+            gbce.record_trade(trade)
+
+        last_tea_stock_trade = sorted(tea_stock_trades,
+                                      key=lambda t: t.timestamp,
+                                      reverse=True)[0]
+        last_gin_stock_trade = sorted(gin_stock_trades,
+                                      key=lambda t: t.timestamp,
+                                      reverse=True)[0]
+
+        current_time = max([last_tea_stock_trade.timestamp,
+                            last_gin_stock_trade.timestamp])
+        tea_stock_price = tea_stock.price(current_time)
+        gin_stock_price = gin_stock.price(current_time)
+        expected_value = (tea_stock_price * gin_stock_price)**(1/2)
+
+        self.assertEqual(gbce.all_share_index(current_time), expected_value)
+
+
+
+
+
